ae4bc202008d8aef0f575a806dba2de08ee6bad8
MuskanSoftTest <muskanambardekar07@gmail.com>
2025-07-07 20:27:26 +0530

    in progress

---
+++ src/test/java/TutorialsNinja/Register/TC_RF_002.java
@@ -0,0 +1,21 @@
+package TutorialsNinja.Register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC_RF_002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.amazon.in/");
+
+		
+	}
+
+}
